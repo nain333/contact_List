7edb8f80661dd12e329a78ad756a0528e1079b0e
@nain333 <hnain70@gmail.com>
2023-03-21 01:03:26 +0530

    create A contact List and send that to the browse

--- index.js
+++ index.js
@@ -4,14 +4,34 @@ const port =8000;
 const app = express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+var contactList=[{
+        name:'Himanshu',
+        phone:1111111111,
+    },
+    {
+        name:'Google',
+        phone:222222222,
+    },
+    {
+        name:'faceBook',
+        phone:3333333333
+    }
+
+]
 app.get('/',function(req,res){
     console.log(req);
-    return res.render('index',{title:'My Contacts'})
+    return res.render('index',{
+        title:'My Contacts',
+        contact_List:contactList
+    })
     
 });
 app.get('/profile',function(req,res){
     console.log(req);
     res.send('<h1>This is profile dude!</h1>');
+})
+app.get('/playground',function(req,res){
+    return res.render('playground',{title:'Playground'});
 })
 
 
